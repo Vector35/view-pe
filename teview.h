2cec579e882a52f9c7153f27b70b006fe40a4d29
Brandon Miller <brandon@vector35.com>
2024-03-07 17:21:23 -0500

    Additional validation for TE loader
    Simple check on VZ header isn't enough. This commit adds additional
    code to check the section headers for a section with a name of .text
    as a second source of validation

--- teview.h
+++ teview.h
@@ -10,8 +10,8 @@
 
 // EFI_IMAGE_DATA_DIRECTORY
 struct TEImageDataDirectory {
-    uint32_t virtualAddress;
-    uint32_t size;
+	uint32_t virtualAddress;
+	uint32_t size;
 };
 
 #define EFI_TE_IMAGE_HEADER_SIZE 40
@@ -24,74 +24,74 @@ struct TEImageDataDirectory {
 
 // EFI_TE_IMAGE_HEADER
 struct TEImageHeader {
-    uint16_t magic;
-    uint16_t machine;
-    uint8_t numberOfSections;
-    uint8_t subsystem;
-    uint16_t strippedSize;
-    uint32_t addressOfEntrypoint;
-    uint32_t baseOfCode;
-    uint64_t imageBase;
-    struct TEImageDataDirectory dataDirectory[2];
+	uint16_t magic;
+	uint16_t machine;
+	uint8_t numberOfSections;
+	uint8_t subsystem;
+	uint16_t strippedSize;
+	uint32_t addressOfEntrypoint;
+	uint32_t baseOfCode;
+	uint64_t imageBase;
+	struct TEImageDataDirectory dataDirectory[2];
 };
 
 // EFI_IMAGE_SECTION_HEADER
 struct TEImageSectionHeader {
-    std::string name; // 8 bytes
-    uint32_t virtualSize;
-    uint32_t virtualAddress;
-    uint32_t sizeOfRawData;
-    uint32_t pointerToRawData;
-    uint32_t pointerToRelocations;
-    uint32_t pointerToLineNumbers;
-    uint16_t numberOfRelocations;
-    uint16_t numberOfLineNumbers;
-    uint32_t characteristics;
+	std::string name; // 8 bytes
+	uint32_t virtualSize;
+	uint32_t virtualAddress;
+	uint32_t sizeOfRawData;
+	uint32_t pointerToRawData;
+	uint32_t pointerToRelocations;
+	uint32_t pointerToLineNumbers;
+	uint16_t numberOfRelocations;
+	uint16_t numberOfLineNumbers;
+	uint32_t characteristics;
 };
 
 namespace BinaryNinja
 {
-    class TEView: public BinaryView
-    {
-        bool m_parseOnly;
-        std::vector<TEImageSectionHeader> m_sections;
-        bool m_relocatable = false;
-        Ref<Logger> m_logger;
-        bool m_backedByDatabase;
-        uint64_t m_imageBase;
-        Ref<Architecture> m_arch;
-        uint64_t m_entryPoint;
-        
-    protected:
-        virtual uint64_t PerformGetEntryPoint() const override;
-        virtual bool PerformIsExecutable() const override { return true; }
-        virtual BNEndianness PerformGetDefaultEndianness() const override { return LittleEndian; }
-        virtual bool PerformIsRelocatable() const override { return m_relocatable; }
-        virtual size_t PerformGetAddressSize() const override;
+	class TEView: public BinaryView
+	{
+		bool m_parseOnly;
+		std::vector<TEImageSectionHeader> m_sections;
+		bool m_relocatable = false;
+		Ref<Logger> m_logger;
+		bool m_backedByDatabase;
+		uint64_t m_imageBase;
+		Ref<Architecture> m_arch;
+		uint64_t m_entryPoint;
+		
+	protected:
+		virtual uint64_t PerformGetEntryPoint() const override;
+		virtual bool PerformIsExecutable() const override { return true; }
+		virtual BNEndianness PerformGetDefaultEndianness() const override { return LittleEndian; }
+		virtual bool PerformIsRelocatable() const override { return m_relocatable; }
+		virtual size_t PerformGetAddressSize() const override;
 
-    public:
-        TEView(BinaryView* data, bool parseOnly = false);
-        virtual bool Init() override;
+	public:
+		TEView(BinaryView* data, bool parseOnly = false);
+		virtual bool Init() override;
 
-    private:
-        void ReadTEImageHeader(BinaryReader& reader, struct TEImageHeader& imageHeader);
-        void ReadTEImageSectionHeaders(BinaryReader& reader, uint32_t numSections);
-        void HandleUserOverrides();
-        void CreateSections();
-        void AssignHeaderTypes();
-    };
+	private:
+		void ReadTEImageHeader(BinaryReader& reader, struct TEImageHeader& imageHeader);
+		void ReadTEImageSectionHeaders(BinaryReader& reader, uint32_t numSections);
+		void HandleUserOverrides();
+		void CreateSections();
+		void AssignHeaderTypes();
+	};
 
-    class TEViewType: public BinaryViewType
-    {
+	class TEViewType: public BinaryViewType
+	{
 		Ref<Logger> m_logger;
 
-    public:
-        TEViewType();
-        virtual Ref<BinaryView> Create(BinaryView* data) override;
-        virtual Ref<BinaryView> Parse(BinaryView* data) override;
-        virtual bool IsTypeValidForData(BinaryView* data) override;
-        virtual Ref<Settings> GetLoadSettingsForData(BinaryView* data) override;
-    };
+	public:
+		TEViewType();
+		virtual Ref<BinaryView> Create(BinaryView* data) override;
+		virtual Ref<BinaryView> Parse(BinaryView* data) override;
+		virtual bool IsTypeValidForData(BinaryView* data) override;
+		virtual Ref<Settings> GetLoadSettingsForData(BinaryView* data) override;
+	};
 
-    void InitTEViewType();
+	void InitTEViewType();
 }
